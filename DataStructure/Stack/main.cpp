2a43857d5cf44d18ce7f8797ad00c990b05809a1
Eugene <eugene2165@gmail.com>
2023-10-25 01:16:32 +0800

    Data Structure

---
+++ DataStructure/Stack/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include"Stack.h"
+
+using namespace std;
+
+int main(){
+    Stack_Array<int> data;
+    for(int i = 0; i < 5; i++){
+        data.Push(i);
+        data.Print_Stack();
+    }
+    for(int i = 0; i < 5; i++){
+        cout << data.Top() << " ";
+        data.Print_Stack();
+        data.Pop();
+    }
+
+    return 0;
+}
