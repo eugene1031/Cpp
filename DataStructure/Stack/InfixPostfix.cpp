3b2906b4e034973332753b7faf0595ecc1bbd798
Eugene <eugene2165@gmail.com>
2023-11-07 11:48:59 +0800

    Data Structure

---
+++ DataStructure/Stack/InfixPostfix.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main(){
+    string str = "((a+(b*c))+d)";
+    stack<char> sign;
+
+    for (char c : str)
+    {
+        if(c == '('){
+            continue;
+        }
+        else if(c >= 'a' && c <= 'z'){
+            cout << c;
+        }
+        else if(c == ')'){
+            cout << sign.top();
+            sign.pop();
+        }
+        else{
+            sign.push(c);
+        }
+    }
+    return 0;
+}
